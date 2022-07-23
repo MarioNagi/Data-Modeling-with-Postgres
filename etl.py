3e2353c3f4dc0f6c249838ac51e24e579d8f191e
MarioNagi <Mario_Nagi@yahoo.com>
2022-07-23 22:49:14 +0200

    Add files via upload

--- etl.py
+++ etl.py
@@ -3,13 +3,22 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+from sqlalchemy_schemadisplay import create_schema_graph
+from sqlalchemy import MetaData
 
 def process_song_file(cur, filepath):
-    
+    """
+    This procedure processes a song file whose filepath has been provided as an arugment.
+    It extracts the song information in order to store it into the songs table.
+    Then it extracts the artist information in order to store it into the artists table.
+
+    INPUTS: 
+    * cur the cursor variable
+    * filepath the file path to the song file
+    """
     # open song file
     df = pd.read_json(filepath, dtype={'year': int}, lines=True)
-
+    
     # insert song record
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
@@ -20,9 +29,18 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """
+    This procedure processes a log file whose filepath has been provided as an arugment.
+    It extracts the time information in order to store it into the time table.
+    Then it extracts the user information in order to store it into the user table.
 
+    INPUTS: 
+    * cur the cursor variable
+    * filepath the file path to the song file
+    """
     # open log file
     df = pd.read_json(filepath, lines=True)
+   
 
     # filter by NextSong action
     df = df[df['page'] == 'NextSong'].copy()
@@ -66,7 +84,15 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    This procedure takes the conn, cur, filepath and func to start processing json files in either songs or log directories 
 
+    INPUTS: 
+    * conn, the connection to the database
+    * cur the cursor variable
+    * filepath the file path to the song/log file
+    * func, which deterime which function to run (song or log function)
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -88,10 +114,11 @@ def process_data(cur, conn, filepath, func):
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
-
+    
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
+    graph = create_schema_graph(metadata=MetaData('postgresql://student:student@127.0.0.1/sparkifydb'))
+    graph.write_png('sparkifydb_erd.png')
     conn.close()
 
 
